b48745d2eed820d513cf9c78552961c1060346e1
SamuelSanchezGit <samuelsanchezsam@proton.me>
2024-10-27 19:59:27 +0100

    FIX 3

--- learn-js/src/components/codeWorker.js
+++ learn-js/src/components/codeWorker.js
@@ -1,17 +1,17 @@
+/* eslint-disable no-restricted-globals */
 // eslint-disable-next-line no-restricted-globals
 self.onmessage = function (event) {
     const { code } = event.data;
     let result;
   
     try {
-      // Exécution sécurisée du code en utilisant `new Function`
-      const codeToRun = new Function(code);
-      result = codeToRun();
+        // Exécution sécurisée du code en utilisant `new Function`
+        const codeToRun = new Function(code);
+        result = codeToRun();
     } catch (error) {
-      result = error.message;
+        result = error.message;
     }
   
     // eslint-disable-next-line no-restricted-globals
     self.postMessage(result);
-  };
-  
+};
